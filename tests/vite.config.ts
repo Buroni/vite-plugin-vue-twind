c6005ec5dcea894b3d0687f33c951295eb037324
Jake Browning <jake.reid.browning@gmail.com>
2022-05-01 15:36:49 +0100

    add plugin symlink

--- tests/vite.config.ts
+++ tests/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueTwind from "../dist/index.js";
+import vueTwind from "./plugin.js";
 
 export default defineConfig({
   plugins: [vue(), vueTwind()],
