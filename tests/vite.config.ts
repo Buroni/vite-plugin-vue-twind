40f399225439b785a4c1e3624269dd360be8194b
Jake Browning <jake.reid.browning@gmail.com>
2022-05-01 15:43:11 +0100

    add local plugin dep

--- tests/vite.config.ts
+++ tests/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueTwind from "./plugin.js";
+import vueTwind from "vite-plugin-vue-twind/dist";
 
 export default defineConfig({
   plugins: [vue(), vueTwind()],
