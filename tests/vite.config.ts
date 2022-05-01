a1bf3dcb0c0aedd935423c5bf16377500dc23165
Jake Browning <jake.reid.browning@gmail.com>
2022-05-01 20:59:08 +0100

    import from project root

--- tests/vite.config.ts
+++ tests/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueTwind from "vite-plugin-vue-twind/dist";
+import vueTwind from "vite-plugin-vue-twind";
 
 export default defineConfig({
   plugins: [vue(), vueTwind()],
